26cc17c97d20563274aa3a0a04f85d7077801220
rafaeldtx <rafael991_@hotmail.com>
2019-10-10 13:34:24 -0300

    Fixed to partial recipes index

--- spec/features/visitor_visit_homepage_spec.rb
+++ spec/features/visitor_visit_homepage_spec.rb
@@ -24,11 +24,8 @@ feature 'Visitor visit homepage' do
     visit root_path
 
     # expectativas do usuário após a ação
-    expect(page).to have_css('h1', text: recipe.title)
-    expect(page).to have_css('li', text: recipe.recipe_type.name)
-    expect(page).to have_css('li', text: recipe.cuisine.name)
-    expect(page).to have_css('li', text: recipe.difficulty)
-    expect(page).to have_css('li', text: "#{recipe.cook_time} minutos")
+    expect(page).to have_css('h3', text: recipe.title)
+    expect(page).to have_css('h4', text: recipe.recipe_type.name)
   end
 
   scenario 'and view recipes list' do
@@ -54,16 +51,10 @@ feature 'Visitor visit homepage' do
     visit root_path
 
     # expectativas do usuário após a ação
-    expect(page).to have_css('h1', text: recipe.title)
-    expect(page).to have_css('li', text: recipe.recipe_type.name)
-    expect(page).to have_css('li', text: recipe.cuisine.name)
-    expect(page).to have_css('li', text: recipe.difficulty)
-    expect(page).to have_css('li', text: "#{recipe.cook_time} minutos")
+    expect(page).to have_css('h3', text: recipe.title)
+    expect(page).to have_css('h4', text: recipe.recipe_type.name)
 
-    expect(page).to have_css('h1', text: another_recipe.title)
-    expect(page).to have_css('li', text: another_recipe.recipe_type.name)
-    expect(page).to have_css('li', text: another_recipe.cuisine.name)
-    expect(page).to have_css('li', text: another_recipe.difficulty)
-    expect(page).to have_css('li', text: "#{another_recipe.cook_time} minutos")
+    expect(page).to have_css('h3', text: another_recipe.title)
+    expect(page).to have_css('h4', text: another_recipe.recipe_type.name)
   end
 end
