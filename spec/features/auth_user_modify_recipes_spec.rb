e3215d15717ae3f04bde286c3eb8aa74a549fdc3
rafaeldtx <rafael991_@hotmail.com>
2019-10-14 13:47:44 -0300

    Add rubocop/FactoryBot

--- spec/features/auth_user_modify_recipes_spec.rb
+++ spec/features/auth_user_modify_recipes_spec.rb
@@ -2,14 +2,12 @@ require 'rails_helper'
 
 feature 'auth user modify recipes' do
   scenario 'and create succesfully' do
-    user = User.create(email: 'auth@auth.com', password: '12345678')
-    recipe_type = RecipeType.create(name: 'Sobremesa')
-    cuisine = Cuisine.create(name: 'Brasileira')
-    
-    recipe = Recipe.create(title: 'Torta de Morango', recipe_type: recipe_type,
-                           user: user, cuisine: cuisine, difficulty: 'Média',
-                           ingredients: 'Trigo e Morango', cook_time: 50,
-                           cook_method: 'Misture os ingredientes')
+    user = create(:user)
+    recipe_type = create(:recipe_type)
+    cuisine = create(:cuisine)
+
+    recipe = create(:recipe, user: user, recipe_type: recipe_type,
+                    cuisine: cuisine )
 
     expect(recipe.user).to eq user
   end
